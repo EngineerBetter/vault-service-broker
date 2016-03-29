58c670ec4e2cb81d287982056951eaf0e6a3c7e6
Dan Young <dan@ejecteject.co.uk>
2016-03-29 15:55:54 +0100

    new authentication specs

--- spec/catalog_spec.rb
+++ spec/catalog_spec.rb
@@ -5,13 +5,13 @@ describe "Vault Service Broker" do
 
   describe "Get Catalog" do
 
-   def make_request
-     get "/v2/catalog"
-   end
+    def catalog_request
+      get "/v2/catalog"
+    end
 
    context "if auth credentials are missing" do
      it "returns a 401 unauthorized response" do
-       make_request
+       catalog_request
        expect(last_response.status).to eq(401)
      end
    end
@@ -19,7 +19,7 @@ describe "Vault Service Broker" do
    context "if auth credentials are incorrect" do
      it "returns a 401 unauthorized response" do
        authorize "wrong-user", "wrong-password"
-       make_request
+       catalog_request
        expect(last_response.status).to eq(401)
      end
    end
@@ -27,7 +27,7 @@ describe "Vault Service Broker" do
    context "if auth credentials are correct" do
      it "returns a 200 unauthorized response" do
        authorize "admin", "admin"
-       make_request
+       catalog_request
        expect(last_response.status).to eq(200)
      end
    end
