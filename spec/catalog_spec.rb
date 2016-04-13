dd675e2178dec8c423341bf375e5bd4f3986f85a
Daniel Jones <daniel.jones@engineerbetter.com>
2016-04-13 11:34:47 +0100

    require spec_helper in default RSpec config

--- spec/catalog_spec.rb
+++ spec/catalog_spec.rb
@@ -1,6 +1,3 @@
-# spec/app_spec.rb
-require File.expand_path '../spec_helper.rb', __FILE__
-
 describe "Get Catalog" do
 
   def catalog_request
--- spec/provision_spec.rb
+++ spec/provision_spec.rb
@@ -1,6 +1,3 @@
-# spec/app_spec.rb
-require File.expand_path '../spec_helper.rb', __FILE__
-
 describe "Provision Vault" do
 
   before do
